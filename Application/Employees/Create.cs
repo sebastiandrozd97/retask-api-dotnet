4697017a347250dd89a00ecf2dff63096fe0d2af
sebastiandrozd97 <sebastian.drozd97@gmail.com>
2020-12-02 18:50:31 +0100

    feature: add Create handler for Employee

---
+++ Application/Employees/Create.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Employees
+{
+  public class Create
+  {
+    public class Command : IRequest
+    {
+      public Guid Id { get; set; }
+      public string FirstName { get; set; }
+      public string LastName { get; set; }
+      public string Telephone { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Command>
+    {
+      private readonly DataContext _context;
+      public Handler(DataContext context)
+      {
+        _context = context;
+      }
+
+      public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+      {
+        var employee = new Employee
+        {
+            Id = request.Id,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Telephone = request.Telephone
+        };
+
+        _context.Employees.Add(employee);
+        var success = await _context.SaveChangesAsync() > 0;
+
+        if(success) {
+            return Unit.Value;
+        }
+
+        throw new Exception("Problem saving changes");
+      }
+    }
+  }
+}
