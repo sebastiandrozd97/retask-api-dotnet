7f144c1232d26487d9baeee8704f33e35fb11141
sebastiandrozd97 <sebastian.drozd97@gmail.com>
2020-12-14 15:39:03 +0100

    feature: configure user manager and seed users

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -1,13 +1,44 @@
 using Domain;
+using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Persistence
 {
   public class Seed
   {
-    public static void SeedData(DataContext context)
+    public static async Task SeedData(DataContext context, UserManager<AppUser> userManager)
     {
+      if (!userManager.Users.Any())
+      {
+        var users = new List<AppUser>
+        {
+          new AppUser
+          {
+            DisplayName = "Seba",
+            UserName = "seba",
+            Email = "seba@test.com"
+          },
+          new AppUser
+          {
+            DisplayName = "Piotr",
+            UserName = "piotr",
+            Email = "piotr@test.com"
+          },
+          new AppUser
+          {
+            DisplayName = "Artur",
+            UserName = "artur",
+            Email = "artur@test.com"
+          },
+        };
+        foreach (var user in users)
+        {
+          await userManager.CreateAsync(user, "Passw0rd!");
+        }
+      }
+
       if (!context.Employees.Any())
       {
         var employees = new List<Employee>
